387a35810254f93aa37e760b7e45de7d994ddc70
Zhiyu Guo <zg915@nyu.edu>
2025-03-31 02:10:54 -0400

    debug

--- hw3/truthfulqa.py
+++ hw3/truthfulqa.py
@@ -244,7 +244,7 @@ class MultipleChoicePipeline(Pipeline):
         nll_per_token = nn.functional.cross_entropy(logits_flat, token_ids_flat, reduction="none")
         # nll_per_token = self.loss_fn(logits_flat, token_ids_flat)
         pad_token_id = self.tokenizer.pad_token_id
-        mask = (token_ids_flat != pad_token_id).float()
+        mask = (token_ids_flat != 50256).float()
 
         masked_nll_per_token = nll_per_token * mask
 
@@ -252,7 +252,7 @@ class MultipleChoicePipeline(Pipeline):
         masked_nll_sums = masked_nll_per_token.view(batch_size, seq_len_minus_1).sum(dim=1)
         # shape => [4*x]
 
-        losses_2d = masked_nll_sums.view(batch_size // 4, 4)
+        losses_2d = masked_nll_sums.view(batch_size // self.num_choices, self.num_choices)
 
         predictions = losses_2d.argmin(dim=1)  # shape [x]
 
